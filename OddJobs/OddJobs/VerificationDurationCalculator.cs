2aeedfed01bb1851578fc2605a525cc43c71b2ad
Oliver Kabi <oliver.kabi@carfinance247.co.uk>
2019-09-24 14:27:18 +0100

    Initial commit for yard.

---
+++ OddJobs/OddJobs/VerificationDurationCalculator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace OddJobs
+{
+    public class VerificationDurationCalcualtor
+    {
+        private string filePath = @"C:\Users\Oliver.Kabi\source\repos\hackpad\OddJobs\Application Verification Test.csv";
+        public void GetDurations()
+        {
+            var data = new List<Data>();
+            using (var reader = new StreamReader(filePath))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    data.Add(new Data
+                    {
+                        Token = Guid.Parse(values[0]),
+                        TokenCreated = DateTime.Parse(values[1]).Subtract(TimeSpan.FromHours(23)),
+                        VerifiedAt = DateTime.Parse(values[2])
+                    });
+                }
+            }
+
+        }
+    }
+
+    public class Data
+    {
+        public Guid Token { get; set; }
+        public DateTime TokenCreated { get; set; }
+        public DateTime VerifiedAt { get; set; }
+    }
+}
